0330f6e03c1ae2e28d657c2585a5cd409c8d9c8a
GivenU <agimunimkea@gmail.com>
2024-07-13 12:54:49 +0100

    adding more queries

--- blog.py
+++ blog.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, String, CHAR, ForeignKey, Integer
+from sqlalchemy import create_engine, Column, String, CHAR, ForeignKey, Integer, and_, or_
 from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 import uuid
 
@@ -60,16 +60,19 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 # adding users
-# user1 = User.createUser(session=session, userName="Beatrice_J", firstName="Beatrice", lastName="John")
-# user2 = User.createUser(session=session, userName="PuoloG", firstName="Paul", lastName="Jimmy")
+user1 = User.createUser(session=session, userName="Beatrice_J", firstName="Beatrice", lastName="John")
+user2 = User.createUser(session=session, userName="PuoloG", firstName="Paul", lastName="Jimmy")
 user3 = User.createUser(session=session, userName="Angel", firstName="Simmy", lastName="Dal")
+user4 = User.createUser(session=session, userName="Angel", firstName="Agba", lastName="Dal")
+user5 = User.createUser(session=session, userName="PuoloG", firstName="Clop", lastName="Danny")
 
 # adding posts
-# user1 = Posts.addPost(session=session, userID=user1.userID, postContent="hello, this is my third post")
-# user1 = Posts.addPost(session=session, userID=user1.userID, postContent="hello, this is my fourth post")
-# user2 = Posts.addPost(session=session, userID=user2.userID, postContent="hello, this is my third post")
-# user2 = Posts.addPost(session=session, userID=user2.userID, postContent="hello, this is my fourth post")
+user1 = Posts.addPost(session=session, userID=user1.userID, postContent="hello, this is my third post")
+user1 = Posts.addPost(session=session, userID=user1.userID, postContent="hello, this is my fourth post")
+user2 = Posts.addPost(session=session, userID=user2.userID, postContent="hello, this is my third post")
+user2 = Posts.addPost(session=session, userID=user2.userID, postContent="hello, this is my fourth post")
 
+# querying the data
 user_query = session.query(User).filter_by(firstName="Paul").first()
 for allPosts in user_query.posts:
     print(allPosts.postContent)
@@ -77,3 +80,11 @@ for allPosts in user_query.posts:
 post_query = session.query(Posts).filter_by(postContent="hello, this is my first post").all()
 for all_post in post_query:
     print(all_post.user.userID)
+
+checks = session.query(User).filter(or_(User.userName=="Angel", User.firstName=="Agba")).all()
+for check in checks:
+    print(f"{check.userName}, {check.firstName}, {check.userID}")
+
+checks = session.query(User).filter(and_(User.userName=="Angel", User.lastName=="Simmy")).all()
+for check in checks:
+    print(f"{check.userName}, {check.firstName}, {check.userID}")
