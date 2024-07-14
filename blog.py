688aa3c280f49b395ac69d31e538bcaa1617f16e
GivenU <agimunimkea@gmail.com>
2024-07-14 16:51:56 +0100

    successfully liked the posts

--- blog.py
+++ blog.py
@@ -14,6 +14,7 @@ class User(Base):
     firstName = Column("firstName", String)
     lastName = Column("lastName", String)
     posts = relationship("Posts", back_populates="user")
+    likes = relationship("Likes", back_populates="user")
 
     def __init__(self, userName, firstName, lastName):
         self.userName = userName
@@ -40,6 +41,7 @@ class Posts(Base):
     userID = Column("userID", String, ForeignKey("users.userID"))
     postContent = Column("postContent", String)
     user = relationship("User", back_populates="posts")
+    likes = relationship("Likes", back_populates="post")
 
     def __init__(self, userID, postContent):
         self.userID = userID
@@ -58,16 +60,19 @@ class Likes(Base):
     userID = Column("userID", String, ForeignKey("users.userID"))
     postID = Column("postID", String, ForeignKey("posts.postID"))
     user = relationship("User", back_populates="likes")
-    post = relationship("Post", back_populates="likes")
+    post = relationship("Posts", back_populates="likes")
 
     def __init__(self, userID, postID):
         self.userID = userID
         self.postID = postID
 
-    def addLike(userID, postID):
-        like = Likes(userID, postID)
+    @staticmethod
+    def addLike(session, userID, postID):
+        like = Likes(userID=userID, postID=postID)
         session.add(like)
         session.commit()
+        print(f"A like to {like.user.userName}'s post successfully added")
+        return like
 
 engine = create_engine("sqlite:///blogDB.db")
 Base.metadata.create_all(bind=engine)
@@ -83,36 +88,39 @@ user4 = User.createUser(session=session, userName="Angel", firstName="Agba", las
 user5 = User.createUser(session=session, userName="PuoloG", firstName="Clop", lastName="Danny")
 
 # adding posts
-# user1 = Posts.addPost(session=session, userID=user1.userID, postContent="hello, this is my third post")
+post1 = Posts.addPost(session=session, userID=user1.userID, postContent="this is another post")
 # user1 = Posts.addPost(session=session, userID=user1.userID, postContent="hello, this is my fourth post")
 # user2 = Posts.addPost(session=session, userID=user2.userID, postContent="hello, this is my third post")
 # user2 = Posts.addPost(session=session, userID=user2.userID, postContent="hello, this is my fourth post")
 
+# liking a post
+like1 = Likes.addLike(session=session, userID=user1.userID, postID=post1.postID)
+
 # querying the data
-user_query = session.query(User).filter_by(firstName="Paul").first()
-for allPosts in user_query.posts:
-    print(allPosts.postContent)
+# user_query = session.query(User).filter_by(firstName="Paul").first()
+# for allPosts in user_query.posts:
+#     print(allPosts.postContent)
 
-post_query = session.query(Posts).filter_by(postContent="hello, this is my first post").all()
-for all_post in post_query:
-    print(all_post.user.userID)
+# post_query = session.query(Posts).filter_by(postContent="hello, this is my first post").all()
+# for all_post in post_query:
+#     print(all_post.user.userID)
 
-# checking for a usesr with a certain username or firstname
-checks = session.query(User).filter(or_(User.userName=="Angel", User.firstName=="Agba")).all()
-for check in checks:
-    print(f"{check.userName}, {check.firstName}, {check.userID}")
+# # checking for a usesr with a certain username or firstname
+# checks = session.query(User).filter(or_(User.userName=="Angel", User.firstName=="Agba")).all()
+# for check in checks:
+#     print(f"{check.userName}, {check.firstName}, {check.userID}")
 
-# checking for a user with a certain username and lastname
-checks = session.query(User).filter(and_(User.userName=="Angel", User.lastName=="Simmy")).all()
-for check in checks:
-    print(f"{check.userName}, {check.firstName}, {check.userID}")
+# # checking for a user with a certain username and lastname
+# checks = session.query(User).filter(and_(User.userName=="Angel", User.lastName=="Simmy")).all()
+# for check in checks:
+#     print(f"{check.userName}, {check.firstName}, {check.userID}")
 
-# checking for all posts made by a particular user
-checkAllPost = session.query(User).filter_by(userName="Beatrice_J").first()
-if checkAllPost:
-    print(f"{checkAllPost.firstName}, {checkAllPost.userID}")
+# # checking for all posts made by a particular user
+# checkAllPost = session.query(User).filter_by(userName="Beatrice_J").first()
+# if checkAllPost:
+#     print(f"{checkAllPost.firstName}, {checkAllPost.userID}")
 
-    for post in checkAllPost.posts:
-        print(f"{post.postContent}")
-else:
-    print("User not found!")
+#     for post in checkAllPost.posts:
+#         print(f"{post.postContent}")
+# else:
+#     print("User not found!")
